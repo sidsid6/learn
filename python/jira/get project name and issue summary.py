801c660ff21cb3466824257cec699cb6f30a9490
sidsid6 <35052402+sidsid6@users.noreply.github.com>
2020-03-10 00:34:37 +0900

    Update get project name and issue summary.py

--- python/jira/get project name and issue summary.py
+++ python/jira/get project name and issue summary.py
@@ -10,12 +10,19 @@ options = {
 
 jir = JIRA(server, basic_auth=(user,apikey))
 
+#프로젝트 명
 for project in jir.projects():
     print(project)
-    
+
+#프로젝트 issue key  
 jql = 'project = PRC1'
 issues=jir.search_issues(jql, maxResults=10000)
 
 for issue in issues :
     issue = jir.issue(issue)
     print(issue)
+
+#이슈 key로 summary 가져오기
+iss = jir.issue('prc1-2')
+sum = iss.fields.summary
+print(sum)
