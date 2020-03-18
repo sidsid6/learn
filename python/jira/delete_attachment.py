0a0daf26ac6509894c51cb2ffabd925eced073e8
sidsid6 <35052402+sidsid6@users.noreply.github.com>
2020-03-19 04:32:42 +0900

    Update delete_attachment.py

--- python/jira/delete_attachment.py
+++ python/jira/delete_attachment.py
@@ -25,8 +25,8 @@ for i in query['issues']:
         print("For issue {0}, attachment_name: '{1}', attachment_id: [{2}].".format(i['key'], a['filename'], a['id']))
         
         #ex) For issue PRC1-4, attachment_name: 'flower.png', attachment_id: [10006]
-             For issue PRC1-4, attachment_name: 'flower1.png', attachment_id: [10007]
-             For issue PRC1-4, attachment_name: 'flower2.png', attachment_id: [10008] <- 제거원함
+        #    For issue PRC1-4, attachment_name: 'flower1.png', attachment_id: [10007]
+        #    For issue PRC1-4, attachment_name: 'flower2.png', attachment_id: [10008] <- 제거원함
              
              
 #삭제하고자 하는 첨부파일 id 확인 후, 제거
@@ -39,7 +39,7 @@ for i in range(0,len(query['issues'][0]['fields']['attachment'])):
         pass
 
 #file명으로 제거하기
-attach_name=input("제거할 첨부파일유형 포함 파일명을 입력하세요.(ex: abc.png) : ")
+attach_name=input("제거할 첨부파일유형 포함 파일명을 입력하세요.(ex: abc.png) : ")  # flower2.png 
 for i in range(0,len(query['issues'][0]['fields']['attachment'])):
     if query['issues'][0]['fields']['attachment'][i]['filename']==attach_name:
         jira.delete_attachment(query['issues'][0]['fields']['attachment'][i]['id'])
